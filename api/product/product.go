016eb878ef3df37b83afdae6b36b49fd70cfc952
Cem Tanrikut <cemtanrikut@gmail.com>
2022-04-22 19:41:06 +0300

    Add product type

--- api/product/product.go
+++ api/product/product.go
@@ -1,1 +1,13 @@
 package product
+
+type Product struct {
+	ProductID   string  `json:"product_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Category    string  `json:"category"`
+	Condition   string  `json:"condition"`
+	Prize       float64 `json:"prize"`
+	IsSold      bool    `json:"is_sold"`
+	IsActive    bool    `json:"is_active"`
+	CreateDate  string  `json:"create_date"`
+}
