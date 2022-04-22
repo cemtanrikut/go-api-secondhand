364704b3f11ea62a997c348c2789b2087980f054
Cem Tanrikut <cemtanrikut@gmail.com>
2022-04-22 19:35:46 +0300

    Add customer type

--- api/customer/customer.go
+++ api/customer/customer.go
@@ -1,1 +1,19 @@
 package customer
+
+type Customer struct {
+	CustomerID   string   `json:"customer_id"`
+	Name         string   `json:"name"`
+	Surname      string   `json:"surname"`
+	Email        string   `json:"email"`
+	Password     string   `json:"password"`
+	Phone        string   `json:"phone"`
+	CustomerType bool     `json:"customer_type"` //Person or shop
+	Location     Location `json:"location"`
+}
+
+type Location struct {
+	Country  string `json:"country"`
+	City     string `json:"city"`
+	District string `json:"district"`
+	Address  string `json:"address"`
+}
