089cdc98964d3cd4a7077e110a664f20ae6a6064
Cem Tanrikut <cemtanrikut@gmail.com>
2022-04-22 19:54:34 +0300

    Add customer functions

--- api/customer/customer.go
+++ api/customer/customer.go
@@ -1,5 +1,11 @@
 package customer
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
 type Customer struct {
 	CustomerID   string   `json:"customer_id"`
 	Name         string   `json:"name"`
@@ -9,6 +15,7 @@ type Customer struct {
 	Phone        string   `json:"phone"`
 	CustomerType bool     `json:"customer_type"` //Person or shop
 	Location     Location `json:"location"`
+	IsActive     bool     `json:"is_active"`
 }
 
 type Location struct {
@@ -16,4 +23,36 @@ type Location struct {
 	City     string `json:"city"`
 	District string `json:"district"`
 	Address  string `json:"address"`
+}
+
+func AddCustomer(customer Customer) error {
+	uuid := uuid.New()
+	customer.CustomerID = uuid.String()
+	return nil
+}
+
+func UpdateCustomer(customer Customer) error {
+	customerID := customer.CustomerID
+	updatedCustomer := Customer{
+		CustomerID:   customerID,
+		Name:         customer.Name,
+		Surname:      customer.Surname,
+		Email:        customer.Email,
+		Password:     customer.Password,
+		Phone:        customer.Phone,
+		CustomerType: customer.CustomerType,
+		Location:     customer.Location,
+	}
+	fmt.Println(updatedCustomer)
+	return nil
+}
+
+func DeleteCustomer(customerID string) error {
+	//is_active = false
+	return nil
+}
+
+func GetCustomer(customerID string) (Customer, error) {
+
+	return Customer{}, nil
 }
