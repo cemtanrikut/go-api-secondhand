be6673f28a69d3dd9232b0bbd38267d50bf771da
Cem Tanrikut <cemtanrikut@gmail.com>
2022-04-22 20:05:25 +0300

    Add product type

--- api/product/product.go
+++ api/product/product.go
@@ -1,5 +1,11 @@
 package product
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
 type Product struct {
 	ProductID   string  `json:"product_id"`
 	Name        string  `json:"name"`
@@ -10,4 +16,57 @@ type Product struct {
 	IsSold      bool    `json:"is_sold"`
 	IsActive    bool    `json:"is_active"`
 	CreateDate  string  `json:"create_date"`
+}
+
+func AddProduct(product Product) error {
+	uuid := uuid.New()
+	product.ProductID = uuid.String()
+	//...
+	return nil
+}
+
+func UpdateProduct(product Product) error {
+	productID := product.ProductID
+	updatedProduct := Product{
+		ProductID:   productID,
+		Name:        product.Name,
+		Description: product.Description,
+		Category:    product.Category,
+		Condition:   product.Condition,
+		Prize:       product.Prize,
+		IsSold:      product.IsSold,
+		IsActive:    product.IsActive,
+	}
+	fmt.Println(updatedProduct)
+	return nil
+}
+
+func DeleteProduct(productID string) error {
+	//is_active = false
+	return nil
+}
+
+func GetProduct(productID string) (Product, error) {
+
+	return Product{}, nil
+}
+
+func GetProductsByCustomer(customerID string) ([]Product, error) {
+
+	return []Product{}, nil
+}
+
+func GetProductsByCategory(category string) ([]Product, error) {
+
+	return []Product{}, nil
+}
+
+func GetProductsWithFilters(filter map[string]string) ([]Product, error) {
+
+	return []Product{}, nil
+}
+
+func GetProducts() ([]Product, error) {
+
+	return []Product{}, nil
 }
